7e84605e2cdad9e764d5c99b18e6fe76f4acc5b9
Andrea Dieni <andrea.dieni@gmail.com>
2019-12-03 10:28:27 +0100

    Download from my fork

--- X50.sh
+++ X50.sh
@@ -6,7 +6,7 @@ macos_tools=$repo_dir/macos-tools
 
 if [[ ! -d $macos_tools ]]; then
     echo "Downloading latest macos-tools..."
-    rm -Rf $macos_tools && git clone https://github.com/the-braveknight/macos-tools $macos_tools --quiet
+    rm -Rf $macos_tools && git clone https://github.com/AD2076/macos-tools $macos_tools --quiet
 fi
 
 source $macos_tools/_hack_cmds.sh
